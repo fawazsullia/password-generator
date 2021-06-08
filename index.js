faecaa927cbbcc63ef0c27267f71c718f4c5378c
Fawaz Sullia <fawazsullia@gmail.com>
2021-06-08 18:06:30 +0530

    adsa

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const cors = require('cors');
 require('dotenv').config()
 
 
-const PORT = process.env.PORT || 5000;
-
-
 const password = require('./generate-password.js');
 
 app.use(express.json());
@@ -27,4 +24,4 @@ res.status(200).json({ data : passtoSend}).end();
 
 
 
-app.listen(PORT, ()=> console.log("up and running bitch"));
+app.listen(process.env.PORT, ()=> console.log("up and running bitch"));
